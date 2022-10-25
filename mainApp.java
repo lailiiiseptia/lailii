f613b5b3acd003e547a570e9c5a425db381655b4
lailiiiseptia <114505316+lailiiiseptia@users.noreply.github.com>
2022-10-25 23:37:09 +0800

    Add files via upload

---
+++ mainApp.java
@@ -0,0 +1,19 @@
+public class mainApp {
+    public static void main(String[] args){
+        karyawan karyawan=new karyawan("Laili septia", "gerintuk", "08");
+
+        penggajian penggajian=new penggajian("Rp.10.700.000", "Rp.2.400.000", "Rp.4.500.000");
+
+        status_pendidikan status_pendidikan=new status_pendidikan("Aktif");
+
+        jabatan jabatan=new jabatan("Dosen");
+
+        karyawan.cetakkaryawan(karyawan.no_hp);
+        penggajian.cetakpenggajian(penggajian.gajiPokok);
+        jabatan.cetakjabatan(jabatan.jabatan);
+        status_pendidikan.cetakstatus_pendidikan(status_pendidikan.status);
+    }
+
+
+}
+
